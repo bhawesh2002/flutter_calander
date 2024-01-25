d39ab488de756544fdebe9970c8f8ad47e9205f2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-01-25 21:57:41 +0530

    create class flutter_calander

--- lib/flutter_calander.dart
+++ lib/flutter_calander.dart
@@ -1,7 +1,7 @@
+// ignore_for_file: camel_case_types
+
 library flutter_calander;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'package:flutter/material.dart';
+
+class flutter_calander extends StatefulWidget {}
